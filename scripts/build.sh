20c6f4446d683c9608ad9cbd93b72f29bde30e97
woudini <wukl011@gmail.com>
2017-10-10 14:29:51 -0400

    example build

---
+++ scripts/build.sh
@@ -0,0 +1,22 @@
+devFile=$1
+featureName=$2
+
+IFS=$'
+'
+
+nl='
+'
+
+cd ../$featureName/;
+baseStrings=( $(grep "." base) );
+
+target="$(cat $devFile)";
+
+for f in Translations/*; do
+  fileName=${f##*/};
+  translatedStrings=( $(grep "." Translations/$fileName) );
+  for i in "${!translatedStrings[@]}"; do
+    target=$(echo $target | sed s/${baseStrings[$i]}/${translatedStrings[$i]}/g);
+  done
+  echo "$target";
+done
